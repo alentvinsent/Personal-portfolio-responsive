8ad6e63c958058d61a4f237d20972dfcc8a4738c
Apple <apple@Apples-MacBook-Air.local>
2022-08-23 11:45:58 +0530

    first commit

---
+++ js/formValidation.js
@@ -0,0 +1,12 @@
+<script>
+    function validateform(){
+        var name=document.myform.name.value;
+        var email =document.email.value;
+        if(name==null || name==""){
+            alert('Name box not be blank')
+        }
+        else if(){
+
+        }
+    }
+</script>
